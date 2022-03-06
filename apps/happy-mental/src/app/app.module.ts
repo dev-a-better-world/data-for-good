88eb47106b05ffebc408b1884fed454b26e1d8a0
yonatankra <yonatan.kra@vonage.com>
2022-03-06 17:02:17 +0200

    add vivid

--- apps/happy-mental/src/app/app.module.ts
+++ apps/happy-mental/src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import '@vonage/vwc-button';
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
