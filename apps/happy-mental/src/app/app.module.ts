6f9203dd31ac9ccc54b4b76e489fee0aa51228ef
yonatankra <yonatan.kra@vonage.com>
2022-03-06 20:44:00 +0200

    Fix the app

--- apps/happy-mental/src/app/app.module.ts
+++ apps/happy-mental/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { OverviewComponent } from './overview/overview.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserTableComponent } from './components/user-table/user-table.component';
+import { PieComponent } from './components/charts/pie/pie.component';
+import {NgxChartsModule} from "@swimlane/ngx-charts";
 
 import "@vonage/vwc-list";
 import "@vonage/vwc-side-drawer";
@@ -15,18 +17,13 @@ import "@vonage/vwc-icon";
 import '@vonage/vwc-list/vwc-list-item';
 import "@vonage/vwc-data-grid"
 import "@vonage/vwc-data-grid/vwc-data-grid-column"
-import { AppRoutingModule } from './app-routing.module';
-import { GraphComponent } from './components/graph/graph.component';
-import { UserTableComponent } from './components/user-table/user-table.component';
-
 import '@vonage/vwc-button';
 import '@vonage/vwc-card';
-import { OverviewGraphComponent } from './components/overview-graph/overview-graph.component';
-import { PieComponent } from './components/charts/pie/pie.component';
-import {NgxChartsModule} from "@swimlane/ngx-charts";
+
+import {createCustomElement} from "@angular/elements";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, OverviewComponent, PieComponent, UserTableComponent, OverviewGraphComponent],
+  declarations: [AppComponent, NxWelcomeComponent, OverviewComponent, PieComponent, UserTableComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, NgxChartsModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
